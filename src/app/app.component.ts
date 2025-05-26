711a42f87f87ab011c52f5ee17801a546b710b00
emeraldBD <emerald@bluedrive.ph>
2025-05-26 18:03:39 +0800

    refactor: remove unused code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
-import { Component, OnInit } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,31 +6,4 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent implements OnInit {
-
-  ngOnInit(): void {
-    // startBodyBackgroundCycle(1500);
-  }
-}
-
-const y2kColors: string[] = [
-  '#ff00ff', // magenta
-  '#00ffff', // cyan
-  '#ffff00', // yellow
-  '#ff66cc', // pink
-  '#6600ff', // purple
-  '#00ff99', // mint
-  '#ff3300', // neon orange
-];
-
-let currentIndex = 0;
-
-export function startBodyBackgroundCycle(interval = 2000): void {
-  const body = document.body;
-  if (!body) return;
-
-  setInterval(() => {
-    body.style.backgroundColor = y2kColors[currentIndex];
-    currentIndex = (currentIndex + 1) % y2kColors.length;
-  }, interval);
-}
+export class AppComponent {}
